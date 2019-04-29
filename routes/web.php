bab50a153bf3529cf774c47e34897dc23012cc84
VICIWUOHA <viciwuoha@gmail.com>
2019-04-29 16:03:01 +0100

    socialauth added

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,4 @@ Auth::routes();
 Route::get('/message/{id}','MessageController@create')->name('message');
 Route::post('/message/{id}','MessageController@store')->name('message.send');
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/groupchat', 'HomeController@show');
